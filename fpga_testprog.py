c494e873ac9e2d25357d962012f4ce7d217d3563
robbiedantonio <robbie.dantonio@gmail.com>
2023-11-20 17:03:10 -0500

    Testing multiple functions

--- fpga_testprog.py
+++ fpga_testprog.py
@@ -19,6 +19,9 @@ print(minimized_SOP_dict)
 fpga = FPGA(num_inputs=6, num_outputs=4, num_luts=10, lut_type=4, input_connectionmat=None, lut_connectionmat=None)
 fpga.init_variables(circuit['inputs'], circuit['outputs'])
 
+fpga.map_function(minimized_SOP_dict['X'], circuit['inputs'], 'X')
+fpga.map_function(minimized_SOP_dict['Y'], circuit['inputs'], 'Y')
+fpga.map_function(minimized_SOP_dict['Z'], circuit['inputs'], 'Z')
 fpga.map_function(minimized_SOP_dict['F'], circuit['inputs'], 'F')
 
 # print(fpga.map_function(minimized_SOP_dict['F']))
